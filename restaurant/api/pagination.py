bcc15d0f3e9a6a936f41a1263e069e4ce51c7286
Montadhar Al Rashid <alrashidmon@gmail.com>
2021-09-16 23:26:58 +0300

    add page_size_query_param PageNumberPagination

--- restaurant/api/pagination.py
+++ restaurant/api/pagination.py
@@ -3,3 +3,4 @@ from rest_framework.pagination import PageNumberPagination as RestFrameworkPageN
 
 class PageNumberPagination(RestFrameworkPageNumberPagination):
     page_size = 100
+    page_size_query_param = 'page_size'
